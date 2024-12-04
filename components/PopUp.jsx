7c92ea3d395044652fca894e50925cae34b78b4a
sonu kumar <sonu36437@gmail.com>
2024-12-04 10:42:08 +0530

    Refactor build.gradle and add PopUp component
    - Removed commented font application from build.gradle.
    - Added keyAlias and keyPassword for debug configuration in build.gradle.
    - Introduced a new PopUp component for displaying favorite songs.

---
+++ components/PopUp.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function PopUp({songId}) {
+  return (
+    <View>
+      <Text>Favrouite</Text>
+    </View>
+  )
+}
